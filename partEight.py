a4d9e5d8296017b372ec39c6d01af4874667bfd0
wirrch <wgb4@kent.ac.uk>
2024-10-09 12:45:32 +0100

    delay

--- partEight.py
+++ partEight.py
@@ -1,15 +1,19 @@
 import random
+import time
 
 inventory = []
 playerName = ""
 
 def main():
-    print("You wake to the sound of a window smashing.")
-    print("Creeping through the dark halls of your flat,")
-    print("you hear scurrying of feet against the carpet behind you")
-    print("and before you can turn to face your intruder,")
-    print("you get knocked to the floor and fall unconscious.")
-    print("You wake, alone in the dry heat of the Brazillian streets.")
-    print("WELCOME TO BRAZIL")
+    input("You wake to the sound of a window smashing.")
+    input("Creeping through the dark halls of your flat,")
+    input("you hear scurrying of feet against the carpet behind you")
+    input("and before you can turn to face your intruder,")
+    input("you get knocked to the floor and fall unconscious.")
+    input("You wake, alone in the dry heat of the Brazilian streets.")
+    gameName = "WELCOME TO BRAZIL"
+    for char in gameName:
+        print(char, end="")
+        time.sleep(0.2)
 
-    
+main()
